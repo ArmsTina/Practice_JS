bf88548ac48f067d4e2c61dd24ffedfbae289c31
ArmsTina <didehddud1005@gmail.com>
2024-09-30 02:37:12 -0700

    comments simplifed

--- Basic/12_must_know_array_functions.js
+++ Basic/12_must_know_array_functions.js
@@ -44,12 +44,55 @@ console.log(members.splice(1, 2)); // ['Young', 'Yang']
 console.log(members); // ['Dong']
 
 
-// Immutable methods
+// Immutable Functions
 members = ['Dong', 'Young', 'Yang'];
 
 /**
  * concat() combines the current array with new elements
  * and returns a new array without modifying the original array.
+ * Similar with push().
  */
 console.log(members.concat('Concat')); // ['Dong', 'Young', 'Yang', 'Concat']
 console.log(members); // ['Dong', 'Young', 'Yang']
+
+/**
+ * slice(x, y) extracts(cut) elements from index x to y (not including y).
+ * It does not modify the original array.
+ */
+console.log(members.slice(0, 1)); // ['Dong']
+console.log(members); // ['Dong', 'Young', 'Yang']
+
+/**
+ * Spread operator
+ * The spread operator (...) allows an array to be expanded into individual elements.
+ */
+console.log(members); // ['Dong', 'Young', 'Yang']
+console.log(...members); // Dong Young Yang
+let newMembers = [...members]; // ['Dong', 'Young', 'Yang']
+
+
+// Useful Functions
+
+/**
+ * join() returns the array elements as a string, separated by a comma by default.
+ * You can pass a parameter to use a different separator (e.g., '/' instead of ',').
+ */
+console.log(members.join()); // Dong,Young,Yang
+console.log(members.join('/')); // Dong/Young/Yang
+
+/**
+ * sort() and reverse()
+ * sort() sorts the array based on ASCII values (works best with strings by default).
+ * reverse() reverses the order of the array.
+ */
+
+console.log(members.sort()); // ['Dong', 'Yang', 'Young']
+console.log(members.reverse()); // ['Young', 'Yang', 'Dong']
+
+// For sorting numbers, pass a compare function.
+// If a - b < 0, a comes before b.
+// If a - b > 0, b comes before a.
+// If a - b = 0, the order stays the same.
+let numbers = [1, 4, 6, 9, 2, 5];
+console.log(numbers.sort((a, b) => a - b)); // [1, 2, 4, 5, 6, 9] (ASC)
+console.log(numbers.sort((a, b) => b - a)); // [9, 6, 5, 4, 2, 1] (DESC)
